8568fa194d6f02f924ddb25e00930a26aa6a5e44
Daniel Di Nardi <c18487682@mytudublin.ie>
2021-10-15 17:30:25 +0100

    Finished Lab4 part 1

---
+++ GE1 Examples 2021/Assets/enemyTankSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyTankSpawner : MonoBehaviour
+{
+    public GameObject enemyTank;
+    float min = -40;
+    float max = 40;
+    int enemyCounter = 0;
+
+    System.Collections.IEnumerator spawnEnemyTank(){
+
+        while(true){
+            if(enemyCounter < 5){
+                float x = Random.Range(min, max);
+                float z = Random.Range(min, max);
+                GameObject newEnemyTank = Instantiate(enemyTank, new Vector3(x, transform.position.y, z), Quaternion.identity);
+                enemyCounter++;
+                yield return new WaitForSeconds(1);
+            }
+            else{
+                yield return null;
+            }
+        }
+    }
+
+    public void OnEnable(){
+        StartCoroutine(spawnEnemyTank());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
